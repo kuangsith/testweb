5e51e7c61e0ac9e1e31cb49e56ceb0a2a2f7fd25
Sittipong T <108988177+kuangsith@users.noreply.github.com>
2023-10-10 15:21:31 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datetime
 import streamlit as st
 import numpy
+import sklearn
 
 # df = pd.DataFrame(columns = ['Age','Income'])
 
@@ -11,7 +12,10 @@ import numpy
 df = pd.read_csv('yoyo.csv')
 df['Income'] = df['Income'].apply(lambda x: round(x,2))
 
+
 with st.expander("Click to see DF"):
+  numrow = st.slider('Specify the number of row',2,20,10)
+  df = df.loc[:numrow]
   st.dataframe(df)
 
 
