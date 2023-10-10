02471e09482b1537d86a78729e7bddf467326d4e
Sittipong T <108988177+kuangsith@users.noreply.github.com>
2023-10-10 14:16:39 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ import datetime
 import streamlit as st
 import numpy
 
-df = pd.DataFrame(columns = ['Age','Income'])
+# df = pd.DataFrame(columns = ['Age','Income'])
 
-for i in range(20):
-  df.loc[len(df)] = [random.randint(15, 60), 10000 + 40000 * random.random()]
+# for i in range(20):
+#   df.loc[len(df)] = [random.randint(15, 60), 10000 + 40000 * random.random()]
+
+df = pd.read_csv('yoyo.csv',index=True)
 
 with st.expander("Section 1"):
   st.dataframe(df)
